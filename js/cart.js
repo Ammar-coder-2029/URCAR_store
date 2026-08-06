7e9ab273aaa371a19eb1407e7c22570731b50761
Ammar-Coder-2029 <eng.ammar2029@gmail.com>
2026-08-06 20:39:18 +0300

    fixe remove from cart btn and user

--- js/cart.js
+++ js/cart.js
@@ -1,8 +1,23 @@
 let allproducts = document.querySelector(".products")
-let cartproduct = JSON.parse(localStorage.getItem("productsInCart"))
+let cartproduct = JSON.parse(localStorage.getItem("productsInCart")) || []
+let userinfo = document.querySelector("#user-info")
+let user = document.querySelector("#user")
+let links = document.querySelector("#links")
 
-if (cartproduct) {
-    function drawItems(){
+
+if (localStorage.getItem("username")) {
+    links.remove()
+    userinfo.style.display = "flex"
+    user.innerHTML = localStorage.getItem("username")
+}
+
+if (cartproduct.length > 0) {
+    
+    drawItems()
+} else {
+    showEmpty()
+}
+function drawItems(){
     let x = cartproduct.map((item)=>{
     return `<div class="product">
             <img src="${item.imgurl}">
@@ -11,22 +26,26 @@ if (cartproduct) {
                 <p>${item.color}</p>
             </div>
             <div class="product-act">
-                <button class="remove-cart bg-blue-200" onclick="check(${item.id})">remove</button>
+                <button class="remove-cart bg-blue-200" onclick="removeFromCart(${item.id})">remove</button>
                 <i class="fa-regular fa-heart fav"></i>
             </div>
         </div>`})
     allproducts.innerHTML = x.join("");
     }
-    drawItems()
-} else {
+
+function removeFromCart(id) {
+    cartproduct = cartproduct.filter((item) => item.id !== id)
+    localStorage.setItem("productsInCart", JSON.stringify(cartproduct))
+    if (cartproduct.length > 0) {
+        drawItems()
+    } else {
+        allproducts.innerHTML = ""
+        showEmpty()
+    }
+}
+function showEmpty() {
     let isempty = document.querySelector(".sec-cart")
     isempty.innerHTML=`<h1>tha cart is empty</h1>`
     let section = document.querySelector(".sec-cart")
     section.style.display = "block"
-}
-let remove = document.querySelector(".remove-cart")
-remove.addEventListener("click",removebtn)
-function removebtn(item) {
-    let checkProduct = products.find((item) => item.id === id)
-    checkProduct = localStorage.remove("productsInCart")
 }
